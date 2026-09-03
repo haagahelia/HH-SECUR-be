75c96cf326bb30b7297ea4d6b5a1060beccd92a5
Janne <bhi256@myy.haaga-helia.fi>
2026-09-03 12:45:42 +0300

    Added username to successful login response

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -42,6 +42,7 @@ export const login = async (req: Request, res: Response) => {
 
     res.json({
         message: "Login successful",
-        token,
+        username: user.username,
+        token
     });
 }
