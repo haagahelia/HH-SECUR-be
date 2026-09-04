c8f2c5176180c86ced1ee126e50f7ab2ee87f21c
Janne <bhi256@myy.haaga-helia.fi>
2026-09-04 18:09:58 +0300

    Added cors functionality and allowed all origins

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import { createRoutes } from "./routes/routes.js";
+
+/*
+const corsOptions = {
+    origin: 'https://example.com'
+}
+*/
 
 export const createServer = () => {
     const app = express ();
 
     app
-        .use(express.json());
+        .use(express.json())
+        .use(cors()); //allows all, change to cors(corsOptions), uncomment corsOptions and define allowed origins to set specific allowed origins
 
     app.get("/status", (req: Request, res: Response) => {
         res.json({ ok: true});
