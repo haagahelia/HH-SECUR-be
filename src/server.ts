552ee0251fb5deebd0d85054ee950944119742da
Janne <bhi256@myy.haaga-helia.fi>
2026-09-09 16:35:59 +0300

    Error handler middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { createRoutes } from "./routes/routes.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 /*
 const corsOptions = {
@@ -11,15 +12,18 @@ const corsOptions = {
 export const createServer = () => {
     const app = express ();
 
+
     app
         .use(express.json())
-        .use(cors()); //allows all, change to cors(corsOptions), uncomment corsOptions and define allowed origins to set specific allowed origins
+        .use(cors()) //allows all, change to cors(corsOptions), uncomment corsOptions and define allowed origins to set specific allowed origins
 
     app.get("/status", (req: Request, res: Response) => {
         res.json({ ok: true});
     });
 
     createRoutes(app);
+
+    app.use(errorHandler);
 
     return app;
 }
